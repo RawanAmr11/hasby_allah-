1ad311657f06f37093d8057e4516ceec11be61fb
Aya Shawky <eng.ayashawky123@gmail.com>
2024-08-24 13:19:58 +0300

    first page

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
             appBar: AppBar(
               leading: const Icon(Icons.list),
